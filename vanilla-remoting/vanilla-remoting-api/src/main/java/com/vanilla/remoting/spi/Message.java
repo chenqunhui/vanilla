abd19056577f1df4f4a7583b3d0daca13d6f3d02
chenqunhui <qchen2@xiaohongshu.com>
2018-02-28 19:05:20 +0800

    add

--- vanilla-remoting/vanilla-remoting-api/src/main/java/com/vanilla/remoting/spi/Message.java
+++ vanilla-remoting/vanilla-remoting-api/src/main/java/com/vanilla/remoting/spi/Message.java
@@ -2,15 +2,12 @@ package com.vanilla.remoting.spi;
 
 import java.io.Serializable;
 
-import lombok.Data;
-
 /**
  * 消息体
  * @author cqh
  *
  */
 
-@Data
 public class Message implements Serializable{
 
 	private static final long serialVersionUID = -1830874220384347423L;
@@ -22,4 +19,46 @@ public class Message implements Serializable{
 	private long timestamp;
 	
 	private boolean completed;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public StringBuilder getData() {
+		return data;
+	}
+
+	public void setData(StringBuilder data) {
+		this.data = data;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	
 }
